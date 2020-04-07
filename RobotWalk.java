0aad620ad32b01bacbf112d79d025178f1d1b840
JEONGCHEOLWOO <tonyprince1228@gmail.com>
2020-04-07 16:06:48 +0900

    RobotWalk 클래스 생성, RobotWalk메소드 및  main메소드 생성

---
+++ RobotWalk.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class RobotWalk here.
+ *
+ * @author (2018315034 정철우, 2018315038 이혜인)
+ * @version (2020/04/07)
+ */
+public class RobotWalk
+{
+    public RobotWalk(){
+    
+    }
+    public static void main(String[] args){
+    
+    }
+}
